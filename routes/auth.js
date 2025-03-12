5195e83dedf159fb324effe46e6d33111852ee23
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-12 20:50:11 +0200

    removed unused imports

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { validateAuthentication, User } = require("../models/users");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
