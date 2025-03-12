ae682c7e0080443fb8031a20272b71a25071151d
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-12 21:29:56 +0200

    deleted unused middleware file

--- middlewares/authorization.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-function authorize(webToken, requiredLevel) {
-  const verify = jwt.verify(webToken, "12345");
-
-  return true;
-}
-
-module.exports = authorize;
