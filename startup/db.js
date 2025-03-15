c55b678220754c706b36cd1d19808d85106a5d88
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-15 22:31:39 +0200

    removed inally clause in db startup

--- startup/db.js
+++ startup/db.js
@@ -8,17 +8,10 @@ const clientOptions = {
 };
 
 async function initializeDB() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
+  // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+  await mongoose.connect(uri, clientOptions);
+  await mongoose.connection.db.admin().command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
 }
 
 module.exports = { initializeDB };
