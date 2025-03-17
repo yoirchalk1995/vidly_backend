5ce2587dd634127b9a63b946aa87e7bc379a58ff
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-17 22:05:34 +0200

    added mongoDB transport, tested and working

--- utils/loggers.js
+++ utils/loggers.js
@@ -1,4 +1,5 @@
 const winston = require("winston");
+require("winston-mongodb");
 
 const logger = winston.createLogger({
   level: "error",
@@ -12,6 +13,11 @@ const logger = winston.createLogger({
       filename: "logs/app.log",
     }),
     new winston.transports.Console({ level: "info" }),
+    new winston.transports.MongoDB({
+      level: "error",
+      collection: "logs",
+      db: "mongodb+srv://yoirchalk1995:yoirchalk1995@cluster0.b2d72.mongodb.net/vidly?retryWrites=true&w=majority&appName=Cluster0",
+    }),
   ],
 });
 
